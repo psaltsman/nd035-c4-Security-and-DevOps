41f9ae5710fe4cd7aa0d484382b0141f6e038f56
Paul Saltsman <paul.c.saltsman@gmail.com>
2021-06-05 11:20:47 -0400

    Updates from practice assignments

--- unitTestStarter/src/test/java/com/udacity/examples/Testing/HelperTest.java
+++ unitTestStarter/src/test/java/com/udacity/examples/Testing/HelperTest.java
@@ -1,8 +1,20 @@
 package com.udacity.examples.Testing;
 
-import junit.framework.TestCase;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import com.udacity.examples.Testing.Helper;
+import java.util.*;
 
 public class HelperTest {
 
-	
+    @Test
+    public void getMergedList() {
+
+        List<String> list = Arrays.asList("1", "2", "3");
+
+        assertEquals("1, 2, 3", Helper.getMergedList(list));
+
+        assertNotEquals("3, 2, 1", Helper.getMergedList(list));
+    }
 }
--- unitTestStarter/src/test/java/com/udacity/examples/Testing/Test1Example.java
+++ unitTestStarter/src/test/java/com/udacity/examples/Testing/Test1Example.java
@@ -2,6 +2,13 @@ package com.udacity.examples.Testing;
 
 import junit.framework.TestCase;
 
+import org.junit.Test;
+
 public class Test1Example extends TestCase {
+
+    @Test
+    public void test() {
+
+    }
 
 }
