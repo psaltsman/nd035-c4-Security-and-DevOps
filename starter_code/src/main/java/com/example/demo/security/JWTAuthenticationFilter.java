521b6fd25402ae6bf25be61e499a7fdba19f5ba3
Paul Saltsman <paul.c.saltsman@gmail.com>
2021-06-09 08:52:06 -0400

    Fixes looping issue when username is not found in the database

--- starter_code/src/main/java/com/example/demo/security/JWTAuthenticationFilter.java
+++ starter_code/src/main/java/com/example/demo/security/JWTAuthenticationFilter.java
@@ -29,7 +29,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
 
-        log.info("JWTAuthenticationFilter");
+        log.info("JWTAuthenticationFilter constructor");
 
         this.authenticationManager = authenticationManager;
     }
@@ -69,7 +69,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
         String token = JWT.create()
             .withSubject(((User) auth.getPrincipal()).getUsername())
-            .withExpiresAt(new Date(System.currentTimeMillis() + 864_000_000))
+            .withExpiresAt(new Date(System.currentTimeMillis() + "864_000_000"))
             .sign(HMAC512(("secretkey9876").getBytes()));
 
         log.info("successfulAuthentication: " + token);
